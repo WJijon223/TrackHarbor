126f54bd67f81584b05aa7f0e319f724e8c7689a
WJijon223 <williamjijon223@gmail.com>
2026-04-14 18:15:43 -0400

    added text and vertical box fields to notes controller class

--- src/main/java/com/trackharbor/trackharbor/controllers/NotesPageController.java
+++ src/main/java/com/trackharbor/trackharbor/controllers/NotesPageController.java
@@ -1,4 +1,13 @@
 package com.trackharbor.trackharbor.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class NotesPageController {
+	@FXML
+	private TextField mainSearch;
+
+	@FXML
+	private VBox cardsContainer;
 }
