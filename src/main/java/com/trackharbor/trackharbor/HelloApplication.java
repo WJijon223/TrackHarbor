eb11c98de85d26f83d6c04f4d3138b38f2b2bd73
WJijon223 <williamjijon223@gmail.com>
2026-04-20 13:51:12 -0400

    fixed login page reference in main application run

--- src/main/java/com/trackharbor/trackharbor/HelloApplication.java
+++ src/main/java/com/trackharbor/trackharbor/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("notes-page.fxml")
+                HelloApplication.class.getResource("login-page.fxml")
         );
 
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
